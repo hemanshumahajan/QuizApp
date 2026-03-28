bbefe469db39b168138202bad29fec806e607614
Hemanshu Mahajan <hemanshumahajan21@gmail.com>
2026-03-28 16:36:53 +0530

    Fix Vercel routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const BASE_URL = import.meta.env.VITE_API_URL;
 const API = `${BASE_URL}/api`;
 
+console.log("BASE_URL:", BASE_URL);
+
 const api = axios.create({
   baseURL: API,
   headers: { 'Content-Type': 'application/json' },
